4c3f6ddec3df0275f57c2e6c97d29bff5ea66021
hyein.seo <hyein.seo@devsisters.com>
2023-03-06 12:31:45 +0900

    Test

---
+++ script.py
@@ -0,0 +1,19 @@
+import argparse
+from dbr_test import main
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--game_code', type=str)
+    parser.add_argument('--env_code', type=str)
+    parser.add_argument('--log_type', type=str)
+    parser.add_argument('--target_date', type=str)
+    parser.add_argument('--config', type=str)
+    parser.add_argument('--config_test', type=str)
+    parser.add_argument('--test', action='store_true', default=False)
+    parsed_args = parser.parse_args()
+
+    print(parsed_args)
+    main(parsed_args)
