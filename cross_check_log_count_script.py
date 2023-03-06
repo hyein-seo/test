1516bf4cf052e044c497c4b9e6ea9d0ca6fe48a4
hyein.seo <hyein.seo@devsisters.com>
2023-03-06 17:31:22 +0900

    Test

--- cross_check_log_count_script.py
+++ cross_check_log_count_script.py
@@ -70,6 +70,7 @@ if __name__ == '__main__':
     parser.add_argument('--env_code', type=str)
     parser.add_argument('--log_type', type=str)
     parser.add_argument('--target_date', type=str)
+    parser.add_argument('--etl_json_start', type=str)
     parser.add_argument('--config', type=str)
     parser.add_argument('--config_test', type=str)
     parser.add_argument('--test', action='store_true', default=False)
